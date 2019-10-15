5d2ff10436ad3169849a8613acad187d18b699ef
critsmet <chrismetzgr@gmail.com>
2019-10-15 10:36:17 -0500

    Hosting to heroku

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :teachers
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students
+  # get '/students', to: 'students#index'
+  # get '/students/:id', to: 'students#show'
+  # does the same as resources :students, only: [:index]
+end
